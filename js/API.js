df0d315a04ff4957e2a789364c7c1e1b22d5fd8c
hzhuazhi <58420632+hzhuazhi@users.noreply.github.com>
2020-06-03 10:52:42 +0800

    Add files via upload

---
+++ js/API.js
@@ -0,0 +1,12 @@
+let API = "http://47.116.98.162:8086";
+// let API = "http://192.168.1.44:8086";
+let Json = ',"agtVer": 1,"clientVer": 1,"clientType": 3';
+
+
+
+let publicMap  = new Map();
+publicMap.set("agtVer",1);
+publicMap.set("clientVer",1);
+publicMap.set("clientType",1);
+
+let storage = window.localStorage;
