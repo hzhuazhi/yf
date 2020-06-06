41e3ccc6041e21b9ef190b9f36070073b5961007
tangzhulong12 <345061813@qq.com>
2020-06-06 12:32:52 +0800

    修改页面信息

--- js/API.js
+++ js/API.js
@@ -10,3 +10,13 @@ publicMap.set("clientVer",1);
 publicMap.set("clientType",1);
 
 let storage = window.localStorage;
+
+
+window.alert = function(name){
+    var iframe = document.createElement("IFRAME");
+    iframe.style.display="none";
+    iframe.setAttribute("src", 'data:text/plain,');
+    document.documentElement.appendChild(iframe);
+    window.frames[0].window.alert(name);
+    iframe.parentNode.removeChild(iframe);
+};
