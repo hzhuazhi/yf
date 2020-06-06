204e0727243831e9f9b5544d0353614e80f2923c
tangzhulong12 <345061813@qq.com>
2020-06-06 12:33:29 +0800

    去掉alert

--- js/API.js
+++ js/API.js
@@ -12,11 +12,3 @@ publicMap.set("clientType",1);
 let storage = window.localStorage;
 
 
-window.alert = function(name){
-    var iframe = document.createElement("IFRAME");
-    iframe.style.display="none";
-    iframe.setAttribute("src", 'data:text/plain,');
-    document.documentElement.appendChild(iframe);
-    window.frames[0].window.alert(name);
-    iframe.parentNode.removeChild(iframe);
-};
